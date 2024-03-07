76f0be2c1837c14f5bf28ddcc4fac5a632018860
VivianPongo <u201920614@upc.edu.pe>
2024-03-06 22:19:50 -0500

    feat: updated pythonGPT

--- PythonGPT/pythonGPT.py
+++ PythonGPT/pythonGPT.py
@@ -11,35 +11,49 @@ import openai
 import time  # for measuring time duration of API calls
 import os
 
-openai.api_key = "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"
+openai.api_key = "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"
+
+chat_history = [] #una lista del historial de mensajes 
+
+while True:
+    prompt = input("ingresa una pregunta o prompt: ")
+
+    if prompt == "exit":
+        break
+    else:
+        chat_history.append({"role": "user", "content": prompt}) #el prompt en el historial
+        response = openai.chat.completions.create(
+                model="gpt-3.5-turbo",
+                messages= chat_history,
+                stream = True,
+                max_tokens= 50,
+         )
+
+        collected_chunks = []
+        collected_messages = []
+            # iterate through the stream of events
+        for chunk in response:
+            collected_chunks.append(chunk)  # save the event response
+            chunk_message = chunk.choices[0].delta.content  # extract the message
+            collected_messages.append(chunk_message)  # save the message
+                
+
+        # clean None in collected_messages
+        collected_messages = [m for m in collected_messages if m is not None]
+        full_reply_content = ''.join([m for m in collected_messages])
 
 
-response = openai.chat.completions.create(
-    model="gpt-3.5-turbo",
-    messages=[
-        {"role": "system", "content": "You are a helpful assistant"},
-        {"role": "user", "content": "Who is Lana del Rey? "}
-    ],
-    stream = True,
-    max_tokens= 50,
+        chat_history.append({"role": "system", "content": full_reply_content}) #la respuesta tambien en el historial
+        print(f"Full conversation received: {full_reply_content}")
+
+        #response_message = response.choices[0].message.content
+        #print(response_message)
 
 
-)
 
-collected_chunks = []
-collected_messages = []
-# iterate through the stream of events
-for chunk in response:
-    collected_chunks.append(chunk)  # save the event response
-    chunk_message = chunk.choices[0].delta.content  # extract the message
-    collected_messages.append(chunk_message)  # save the message
-    
 
-# clean None in collected_messages
-collected_messages = [m for m in collected_messages if m is not None]
-full_reply_content = ''.join([m for m in collected_messages])
-print(f"Full conversation received: {full_reply_content}")
 
-#response_message = response.choices[0].message.content
-#print(response_message)
+
+
+
 
